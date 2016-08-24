cbbe7c7cf3d3c57d4c27b7429dc9cc05bfc0bcb6
andrei <andrey.chernishov@gmail.com>
2016-08-24 19:39:17 +0200

    getting and deleting games by id was added

--- src/main/java/com/conwey/game/GameController.java
+++ src/main/java/com/conwey/game/GameController.java
@@ -1,12 +1,25 @@
 package com.conwey.game;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GameController {
     private static AtomicInteger idSequence = new AtomicInteger(0);
+    private Map<Integer, Game> games = new ConcurrentHashMap<>();
 
     public Game createGame(Game game) {
         game.setId(idSequence.getAndIncrement());
+        game.init();
+        games.put(game.getId(), game);
         return game;
+    }
+
+    public Game getGame(Integer id) {
+        return games.get(id);
+    }
+
+    public void deleteGame(Integer id) {
+        games.remove(id);
     }
 }
--- src/main/java/com/conwey/game/Server.java
+++ src/main/java/com/conwey/game/Server.java
@@ -1,7 +1,10 @@
 package com.conwey.game;
 
 import com.google.gson.Gson;
+import spark.Request;
 
+import static spark.Spark.delete;
+import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class Server {
@@ -13,6 +16,13 @@ public class Server {
             Game game = gson.fromJson(req.body(), Game.class);
             return gameController.createGame(game);
         });
+        get("/game/:id", (req, res) -> gameController.getGame(getId(req)));
+        delete("/game/:id", (req, res) -> gameController.deleteGame(getId(req)));
+
+    }
+
+    private static Integer getId(Request req) {
+        return Integer.valueOf(req.params("id"));
     }
 
 
